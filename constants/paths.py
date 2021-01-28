78d0db64c5e4daaa69096582ecb950ab1e3c0175
daniele21 <daniele.moltisanti@mail.polimi.it>
2021-01-28 09:15:43 +0100

    constants update

--- constants/paths.py
+++ constants/paths.py
@@ -6,3 +6,5 @@ MODELS_DIR = 'resources/models/'
 PARAMS_FILENAMES = {'data': 'data_params.json',
                     'network': 'network_params.json',
                     'training': 'training_params.json'}
+
+GLOVE_PATH = 'resources/embeddings/glove.6B/glove.6B.300d.txt'
