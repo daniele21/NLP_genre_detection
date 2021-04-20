3d418d2678326f939ce7fe7a8c9dc34b8e733af0
daniele21 <danielemoltisanti@live.it>
2021-04-20 13:23:31 +0200

    training function

--- scripts/training/model_training.py
+++ scripts/training/model_training.py
@@ -1,19 +1,22 @@
-from scripts.network.NLP_network import LSTM_network
-from scripts.pipeline.dataset_pipeline import generate_training_dataset
+from tensorflow.python.keras.losses import BinaryCrossentropy
+from tensorflow.python.keras.optimizer_v2.adam import Adam
 
-def train_model(params, callbacks=None):
-    data_params = params['data']
+from scripts.network.NLP_network import LSTM_network
+
+
+def train_model(data_resources, params, callbacks=None):
     network_params = params['network']
     training_params = params['training']
 
-    data_resources = generate_training_dataset(data_params)
     tokenizer = data_resources['tokenizer']
     dataset = data_resources['dataset']
 
     network_params['n_word_tokens'] = len(tokenizer['word2idx'])
     network_params['n_classes'] = len(tokenizer['label2idx'])
+    network_params['optimizer'] = Adam
+    network_params['loss'] = BinaryCrossentropy(from_logits=True)
     # network_params['n_classes'] = 40
-    model = LSTM_network(network_params, True)
+    model = LSTM_network(network_params, compile=True)
 
 
     x_train, y_train = dataset['train']['x'], dataset['train']['y']
@@ -27,7 +30,7 @@ def train_model(params, callbacks=None):
               epochs=epochs,
               validation_data=(x_test, y_test),
               callbacks=callbacks,
-              verbose=0,
+              verbose=1,
               )
 
     return model
