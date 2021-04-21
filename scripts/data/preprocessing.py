38c0c8882b24e19eb014486e69a970fbaaadd2c7
daniele21 <danielemoltisanti@live.it>
2021-04-21 10:17:33 +0200

    introudcing stopword removal

--- scripts/data/preprocessing.py
+++ scripts/data/preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from core.preprocessing.text_preprocessing import remove_punctuations, stem_text, lemmatize_text
+from core.preprocessing.text_preprocessing import remove_punctuations, stem_text, lemmatize_text, remove_stopwords
+
 
 def stem_sentence(sentence):
     new_sentence = ''
@@ -18,12 +19,14 @@ def lemmatize_sentence(sentence):
     return new_sentence
 
 
-def sentence_preprocessing(input_data, stemming=True,
-                                       lemmatization=False,
-                                       lowercase=True,
-                                       preload=None):
+def sentence_preprocessing(input_data,
+                           stemming=True,
+                           lemmatization=False,
+                           lowercase=True,
+                           stopwords=True,
+                           preload=None):
 
-    if(preload is not None):
+    if preload is not None:
         return pd.read_csv(preload, index_col=0)
 
     data = input_data.copy(deep=True)
@@ -31,17 +34,21 @@ def sentence_preprocessing(input_data, stemming=True,
     # REMOVING PUNCTUATIONS
     data['synopsis'] = data['synopsis'].apply(remove_punctuations)
 
+    # LOWERING
+    if lowercase:
+        data['synopsis'] = data['synopsis'].apply(lambda x: str(x).lower())
+
+    # REMOVING STOPWORDS
+    if stopwords:
+        data['synopsis'] = data['synopsis'].apply(remove_stopwords)
+
     # STEMMING
-    if(stemming):
+    if stemming:
         data['synopsis'] = data['synopsis'].apply(stem_sentence)
 
     # LEMMATIZATION
-    if(lemmatization):
+    if lemmatization:
         data['synopsis'] = data['synopsis'].apply(lemmatize_sentence)
-
-    # LOWERING
-    if(lowercase):
-        data['synopsis'] = data['synopsis'].apply(lambda x: str(x).lower())
 
     return data
 
